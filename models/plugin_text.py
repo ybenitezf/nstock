0c149c1f99b0890074c4352eda539b99c7471e47
Yoel Benítez Fonseca <ybenitezf@gmail.com>
2016-12-31 11:24:27 -0500

    better plugin registration

--- models/plugin_text.py
+++ models/plugin_text.py
@@ -14,28 +14,29 @@ if False:
 
 # define tables of this plugin
 def _():
-    if not hasattr(db, 'plugin_text_text'):
-        # configure ckeditor
-        editor = CKEditor(db=db)
-        # definimos la BD
-        tbl = db.define_table(
-            'plugin_text_text',
-            Field('byline', 'string', length=250, default=''),
-            Field('body', 'text', label=T('Content')),
-            Field('item_id', 'string', length=64),
-            auth.signature,
-        )
-        tbl.byline.label = T('By line')
-        tbl.item_id.readable = False
-        tbl.item_id.writable = False
-        tbl.body.requires = IS_NOT_EMPTY()
-        tbl.body.widget = editor.widget
-
-        # enable record  versioning
-        tbl._enable_record_versioning()
     plugins = PluginManager('text', app=None)
     if plugins.text.app is not None:
+        # this will register the content/type on the application
         plugins.text.app.registerContentType('text', ContentText())
+        if not hasattr(db, 'plugin_text_text'):
+            # configure ckeditor
+            editor = CKEditor(db=db)
+            # definimos la BD
+            tbl = db.define_table(
+                'plugin_text_text',
+                Field('byline', 'string', length=250, default=''),
+                Field('body', 'text', label=T('Content')),
+                Field('item_id', 'string', length=64),
+                auth.signature,
+            )
+            tbl.byline.label = T('By line')
+            tbl.item_id.readable = False
+            tbl.item_id.writable = False
+            tbl.body.requires = IS_NOT_EMPTY()
+            tbl.body.widget = editor.widget
 
-    return True
+            # enable record  versioning
+            tbl._enable_record_versioning()
+
+    return
 _()
